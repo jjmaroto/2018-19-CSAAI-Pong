33b1d0d619fa6bfd7972624f57e2f3091ab979e2
Leyree <leyremc@MacBook-Pro-de-Leyree.local>
2019-03-26 19:25:51 +0100

    Sin apenas avances.. Seguir repasando.

--- pong.js
+++ pong.js
@@ -4,7 +4,6 @@ var WIDTH = 600,
     HEIGHT = 400;
 
 function main(){
-  //console.log("Pong: Main: Start!")
 
   var canvas = document.getElementById('display')
   canvas.width = WIDTH;
@@ -20,24 +19,6 @@ function main(){
     i += 15;
   }
 
-  //-- RAQUETAS:
-  var paddles = {
-    init: function(ctx){
-      this.ctx = ctx;
-    },
-
-    draw: function () {
-      ctx.fillStyle = 'white';
-      //-- PLAYER:
-      ctx.fillRect(50, 180, 10, 50);
-      //-- CPU:
-      ctx.fillRect(550, 180, 10, 50);
-    },
-  }
-
-  paddles.init(ctx);
-  paddles.draw();
-
   //-- PUNTUACION:
   var score = {
     score1: 0,
@@ -49,47 +30,124 @@ function main(){
 
     draw: function (){
       this.ctx.font = "50px Arial";
-      this.ctx.fillStyle = 'white';
+      this.ctx.fillStyle = 'PeachPuff';
       //--SCORE1:
       this.ctx.fillText(this.score1, 255,50 );
       //--SCORE2:
       this.ctx.fillText(this.score2, 320,50 );
     },
+
+    reset: function(){
+      this.score1 = 0;
+      this.score2 = 0;
+    }
   }
 
   score.init(ctx);
   score.draw();
 
+  //-- RAQUETA del JUGADOR (izq):
+  var playerPaddles = {
+    //-- Posicion inicial:
+    x_init: 50,
+    y_init: 180,
+
+    //-- Coordenadas:
+    x: 0,
+    y: 0,
+
+    //-- Velocidad de la raqueta:
+    vy: 2,
+
+    //-- Contexto:
+    ctx: null,
+
+    init: function(ctx){
+      this.reset();
+      this.ctx = ctx;
+    },
+
+    draw: function () {
+      ctx.fillStyle = 'white';
+      //-- PLAYER:
+      ctx.fillRect(this.x, this.y, 10, 50);
+    },
+
+    reset: function (){
+      this.x = this.x_init;
+      this.y = this.y_init;
+    },
+  }
+
+  playerPaddles.init(ctx);
+  playerPaddles.draw();
+
+  // -- RAQUETA de la CPU (der):
+  var cpuPaddles = {
+    //-- Posicion inicial:
+    x_ini: 550,
+    y_ini: 180,
+
+    //-- Coordenadas:
+    x: 0,
+    y: 0,
+
+    //-- Velocidad de la raqueta:
+    vy: 2,
+
+    //-- Contexto:
+    ctx: null,
+
+    init: function(ctx){
+      this.reset();
+      this.ctx = ctx;
+    },
+
+    draw: function () {
+      ctx.fillStyle = 'white';
+      ctx.fillRect(this.x, this.y, 10, 50);
+    },
+
+    update: function (){
+      this.y = ball.y - 0.25;
+    },
+
+    reset: function (){
+      this.x = this.x_ini;
+      this.y = this.y_ini;
+    }
+  }
+
+  cpuPaddles.init(ctx);
+  cpuPaddles.draw();
+
+
   //-- BOLA:
   var ball = {
     //-- Posicion INICIAL:
-    x_ini: 300,
+    x_ini: 100,
     y_ini: 200,
-
-    //-- Dimensiones:
-    width: 10,
-    height: 10,
 
     //-- Coordenadas:
     x: 0,
     y: 0,
 
     //-- Velocidad:
-    vx: 4,
-    vy: 1,
+    vx: 5,
+    vy: 2,
 
     //-- Contexto:
     ctx: null,
 
     //-- Inicializar la bola:
     init: function(ctx){
-      this.ctx = ctx;
       this.reset();
+      this.ctx = ctx;
     },
 
     //-- Dibujar la bola:
     draw: function () {
-      this.ctx.fillStyle = 'yellow';
+      this.ctx.fillStyle = 'lightblue';
       //-- BOLA CUADRADA:
       //this.ctx.fillRect(this.x, this.y, this.width, this.height)
       //-- BOLA REDONDA:
@@ -113,16 +171,22 @@ function main(){
   ball.init(ctx)
   ball.draw()
 
-  //-- Crear timer para la animación
-  //-- Inicialmente a null
   var timer = null;
+  var start = document.getElementById('start');
 
-  var sacar = document.getElementById('sacar');
+  //-- Comienza la animación!
+  start.onclick = () => {
 
-  //--CÓDIGO PENDIENTE para la PROXIMA SESION!!
+    if (!timer){
+      timer = setInterval(() =>{
+        ball.update();
+        ctx.clearRect(0,0,canvas.WIDTH, canvas.HEIGHT);
+        ball.draw();
 
-
-
-
-
+      }, 20);
+    }
+  } //-- FIN ONCLICK;
 }
+
+  //-- ESCACIO: 32, FLECHA ARRIBA: 38, FLECHA ABAJO: 40
+  //-- Para consultar más: https://keycode.info
