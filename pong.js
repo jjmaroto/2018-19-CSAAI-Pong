ed1a9acbf35c9dba92fb25b8758d72cb200179c9
Leyree <leyremc@MacBook-Pro-de-Leyree.local>
2019-04-02 12:38:14 +0200

    Nueva implementación de código: metido en funciones.

--- pong.js
+++ pong.js
@@ -3,6 +3,115 @@
 var WIDTH = 600,
     HEIGHT = 400;
 
+function makeScore(WIDTH, HEIGHT){
+  this.ctx = null;
+
+  this.score1 = 0;
+  this.score2 = 0;
+
+  this.width = WIDTH;
+  this.height = HEIGHT;
+
+  this.init =  function(ctx){
+    this.reset();
+    this.ctx = ctx;
+  };
+
+  this.draw = function (){
+    this.ctx.font = "70px American Typewriter";
+    this.ctx.fillStyle = 'gray';
+    //--SCORE1:
+    this.ctx.fillText(this.score1, 240,60 );
+    //--SCORE2:
+    this.ctx.fillText(this.score2, 320,60 );
+
+    //-- RED:
+    var i = 0;
+    while (i < this.height){
+      this.ctx.fillStyle = 'white';
+      this.ctx.fillRect(300, i, 1, 10)
+      i += 15;
+    }
+  };
+
+  this.reset = function(){
+    this.score1 = 0;
+    this.score2 = 0;
+  }
+}
+
+function makePaddles (x,y, HEIGHT){
+  this.ctx = null;
+
+  this.x_ini = x;
+  this.y_ini = y;
+
+  this.vy = 0;
+  this.speed = 5;
+
+  this.init = function(ctx){
+    this.reset();
+    this.ctx = ctx;
+  };
+
+  this.draw = function () {
+    this.ctx.fillStyle = 'black';
+    this.ctx.fillRect(this.x, this.y, 10, 50);
+  };
+
+  this.reset = function (){
+    this.x = this.x_ini;
+    this.y = this.y_ini;
+  };
+
+  this.update = function() {
+    this.y += this.vy*this.speed;
+  };
+}
+
+function makeBall() {
+  this.ctx = null;
+
+  this.x_ini = 85;
+  this.y_ini = 125;
+
+  this.x = 0;
+  this.y = 0;
+
+  this.vx = 5;
+  this.vy = 2;
+
+  this.init = function(ctx){
+    this.reset();
+    this.ctx = ctx;
+  };
+
+  this.draw = function () {
+    this.ctx.fillStyle = 'plum';
+    this.ctx.beginPath();
+    //-- Dibujar un circulo: coordenadas x,y del centro
+    //-- Radio, Angulo inicial y angulo final
+    this.ctx.arc(this.x, this.y, 5, 0, 2 * Math.PI);
+    //-- BOLA CUADRADA:
+    // this.ctx.fillRect(this.x, this.y, this.width, this.height)
+    this.ctx.fill()
+  };
+
+  //-- Update
+  this.update = function () {
+    this.x += this.vx;
+    this.y += this.vy;
+  };
+
+  //-- Reset: Set the ball to the initial state
+  this.reset = function() {
+    this.x = this.x_ini;
+    this.y = this.y_ini;
+
+    this.vy = - this.vy;
+  };
+}
+
 function main(){
 
   var canvas = document.getElementById('display')
@@ -11,168 +120,25 @@ function main(){
 
   var ctx = canvas.getContext("2d");
 
-  //-- RED:
-  var i = 0;
-  while (i<canvas.height){
-    ctx.fillStyle = 'white';
-    ctx.fillRect(300, i, 1, 10)
-    i += 15;
-  }
-
-  //-- PUNTUACION:
-  var score = {
-    score1: 0,
-    score2: 0,
-
-    init: function(ctx){
-      this.ctx = ctx;
-    },
-
-    draw: function (){
-      this.ctx.font = "50px Arial";
-      this.ctx.fillStyle = 'PeachPuff';
-      //--SCORE1:
-      this.ctx.fillText(this.score1, 255,50 );
-      //--SCORE2:
-      this.ctx.fillText(this.score2, 320,50 );
-    },
-
-    reset: function(){
-      this.score1 = 0;
-      this.score2 = 0;
-    }
-  }
+  var score = new makeScore(canvas.width, canvas.height);
 
   score.init(ctx);
   score.draw();
 
-  //-- RAQUETA del JUGADOR (izq):
-  var playerPaddles = {
-    //-- Posicion inicial:
-    x_init: 50,
-    y_init: 180,
+  var paddle1 = new makePaddles(50, 100, canvas.height);
+  var paddle2 = new makePaddles(550, 200, canvas.height);
 
-    //-- Coordenadas:
-    x: 0,
-    y: 0,
+  paddle1.init(ctx);
+  paddle1.draw();
 
-    //-- Velocidad de la raqueta:
-    vy: 2,
+  paddle2.init(ctx);
+  paddle2.draw();
 
-    //-- Contexto:
-    ctx: null,
+  var ball = new makeBall();
 
-    init: function(ctx){
-      this.reset();
-      this.ctx = ctx;
-    },
+  ball.init(ctx);
+  ball.draw();
 
-    draw: function () {
-      ctx.fillStyle = 'white';
-      //-- PLAYER:
-      ctx.fillRect(this.x, this.y, 10, 50);
-    },
-
-    reset: function (){
-      this.x = this.x_init;
-      this.y = this.y_init;
-    },
-  }
-
-  playerPaddles.init(ctx);
-  playerPaddles.draw();
-
-  // -- RAQUETA de la CPU (der):
-  var cpuPaddles = {
-    //-- Posicion inicial:
-    x_ini: 550,
-    y_ini: 180,
-
-    //-- Coordenadas:
-    x: 0,
-    y: 0,
-
-    //-- Velocidad de la raqueta:
-    vy: 2,
-
-    //-- Contexto:
-    ctx: null,
-
-    init: function(ctx){
-      this.reset();
-      this.ctx = ctx;
-    },
-
-    draw: function () {
-      ctx.fillStyle = 'white';
-      ctx.fillRect(this.x, this.y, 10, 50);
-    },
-
-    update: function (){
-      this.y = ball.y - 0.25;
-    },
-
-    reset: function (){
-      this.x = this.x_ini;
-      this.y = this.y_ini;
-    }
-  }
-
-  cpuPaddles.init(ctx);
-  cpuPaddles.draw();
-
-
-  //-- BOLA:
-  var ball = {
-    //-- Posicion INICIAL:
-    x_ini: 100,
-    y_ini: 200,
-
-    //-- Coordenadas:
-    x: 0,
-    y: 0,
-
-    //-- Velocidad:
-    vx: 5,
-    vy: 2,
-
-    //-- Contexto:
-    ctx: null,
-
-    //-- Inicializar la bola:
-    init: function(ctx){
-      this.reset();
-      this.ctx = ctx;
-    },
-
-    //-- Dibujar la bola:
-    draw: function () {
-      this.ctx.fillStyle = 'lightblue';
-
-      ctx.beginPath();
-      //-- Dibujar un circulo: coordenadas x,y del centro
-      //-- Radio, Angulo inicial y angulo final
-      ctx.arc(this.x, this.y, 5, 0, 2 * Math.PI);
-      //-- BOLA CUADRADA:
-      // this.ctx.fillRect(this.x, this.y, this.width, this.height)
-      this.ctx.fill()
-    },
-
-    //-- Update
-    update: function () {
-      this.x += this.vx;
-      this.y += this.vy;
-    },
-
-    //-- Reset: Set the ball to the initial state
-    reset: function() {
-      this.x = this.x_ini;
-      this.y = this.y_ini;
-    },
-  }
-
-  ball.init(ctx)
-  ball.draw()
 
   var timer = null;
   var start = document.getElementById('start');
@@ -182,11 +148,17 @@ function main(){
 
     if (!timer){
       timer = setInterval(() =>{
-
+        //-- Actualizar elementos:
+        paddle1.update();
+        paddle2.update();
         ball.update();
+        //-- Limpiamos el canvas:
         ctx.clearRect(0,0,canvas.width, canvas.height);
+        //-- Dibujamos:
         ball.draw();
-        console.log("Funciona!")
+        paddle1.draw();
+        paddle2.draw();
+        score.draw();
 
       }, 20);
     }
